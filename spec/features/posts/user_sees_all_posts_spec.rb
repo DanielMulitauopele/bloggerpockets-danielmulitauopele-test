09b1f8bc79827b9e590fff2fca19aa845f6312ff
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-04-13 15:42:55 -0600

    Adds test for unpublished posts

--- spec/features/posts/user_sees_all_posts_spec.rb
+++ spec/features/posts/user_sees_all_posts_spec.rb
@@ -6,16 +6,26 @@ describe 'posts index' do
       @author = User.create!(name: 'Dan', email: 'dan@dan.com', password_digest: '234')
       @post1 = @author.posts.create!(title: 'post', body: 'stuff', published: true)
       @post2 = @author.posts.create!(title: 'post2', body: 'stuff2', published: true)
+      @post3 = @author.posts.create!(title: 'post3', body: 'stuff3')
     end
 
     it "should list all posts" do
       visit posts_path
 
       expect(current_path).to eq(posts_path)
+
       expect(page).to have_content(@post1.title)
       expect(page).to have_content(@post1.body)
       expect(page).to have_content(@post2.title)
       expect(page).to have_content(@post2.body)
+    end
+    it "should not list posts that are not published" do
+      visit posts_path
+
+      expect(current_path).to eq(posts_path)
+
+      expect(page).to_not have_content(@post3.title)
+      expect(page).to_not have_content(@post3.body)
     end
   end
 end
