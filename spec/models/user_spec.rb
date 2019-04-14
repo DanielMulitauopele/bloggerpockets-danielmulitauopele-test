b28f24cab16fb3ccaf6ed176705eccb24e4f1375
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-04-14 16:53:39 -0600

    Corrects syntax, passes tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,6 @@ describe User, type: :model do
   describe 'validations' do
     it {should validate_presence_of :name}
     it {should validate_presence_of :email}
-    it {should validate_presence_of :password}
+    it {should validate_presence_of :password_digest}
   end
 end
