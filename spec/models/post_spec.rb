42db397c53963dfaeee9cdfc577e490cbccd8f11
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-04-14 16:57:22 -0600

    Adds relationship tests for user and post models

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,4 +5,9 @@ describe Post, type: :model do
     it {should validate_presence_of :title}
     it {should validate_presence_of :body}
   end
+
+  describe 'relationships' do
+    it {should have_many :comments}
+    it {should belong_to :user}
+  end
 end
