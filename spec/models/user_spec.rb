39075636e1bf7019b00aa9fa71651c58ec301c64
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-04-14 16:57:22 -0600

    Adds relationship tests for user and post models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,9 @@ describe User, type: :model do
     it {should validate_presence_of :email}
     it {should validate_presence_of :password_digest}
   end
+
+  describe 'relationships' do
+    it {should have_many :posts}
+    it {should have_many :comments}
+  end
 end
