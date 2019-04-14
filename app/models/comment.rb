20ff68f59d0949d07a1db6a0834861071525b20d
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-04-14 16:38:18 -0600

    Adds has_many/belongs_to to models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Comment < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, :post
 
   scope :published, -> { where(published: true) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@
 
 class User < ApplicationRecord
   has_secure_password
-  has_many :posts
+  has_many :posts, :comments
 end
