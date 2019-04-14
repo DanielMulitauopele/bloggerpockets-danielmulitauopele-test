def7c1552434c37f5eb1d0fbcb84d68d34702e83
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-04-13 19:16:28 -0600

    Reorganizes routes for more intuitive reading

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :show]  do
-    resources :comments, only: [:index]
-  end
+  root to: "posts#index"
 
   resources :users, only: [:show]
 
-  root to: "posts#index"
+  resources :posts, only: [:index, :show]  do
+    resources :comments, only: [:index]
+  end
 end
